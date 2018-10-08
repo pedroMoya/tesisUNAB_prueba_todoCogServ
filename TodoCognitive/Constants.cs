30a16c4b8ac21f8d803d7c28936235719b3ac2e0
MOYA SANTIBAÑEZ PEDRO A <p.moyasantibaez@uandresbello.edu>
2018-10-07 21:29:08 -0400

    version1

--- TodoCognitive/Constants.cs
+++ TodoCognitive/Constants.cs
@@ -4,17 +4,17 @@
     {
         public static readonly string AuthenticationTokenEndpoint = "https://api.cognitive.microsoft.com/sts/v1.0";
 
-        public static readonly string BingSpeechApiKey = "_key_";
+        public static readonly string BingSpeechApiKey = "key";
         public static readonly string SpeechRecognitionEndpoint = "https://speech.platform.bing.com/speech/recognition/";
         public static readonly string AudioContentType = @"audio/wav; codec=""audio/pcm""; samplerate=16000";
 
-        public static readonly string BingSpellCheckApiKey = "__key__";
+        public static readonly string BingSpellCheckApiKey = "key";
         public static readonly string BingSpellCheckEndpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck";
 
-        public static readonly string TextTranslatorApiKey = "_key_";
+        public static readonly string TextTranslatorApiKey = "key";
         public static readonly string TextTranslatorEndpoint = "https://api.microsofttranslator.com/v2/http.svc/translate";
 
-        public static readonly string FaceApiKey = "_key_";
+        public static readonly string FaceApiKey = "key";
         public static readonly string FaceEndpoint = "https://brazilsouth.api.cognitive.microsoft.com/face/v1.0";
 
         public static readonly string AudioFilename = "Todo.wav";
